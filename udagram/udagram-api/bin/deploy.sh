11f18a5067054eb0e5b74f8736530f8a1eb4da7d
ahmed <Ahmed Mubarak>
2023-01-04 15:45:33 +0200

    reviewer notes

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,14 +1,14 @@
 npm i 
 npm run build 
 eb use udagram-api-dev
-eb setenv POSTGRES_HOST = $POSTGRES_HOST
-eb setenv POSTGRES_DB = $POSTGRES_DB
-eb setenv POSTGRES_USERNAME = $POSTGRES_USERNAME
-eb setenv POSTGRES_PASSWORD = $POSTGRES_PASSWORD
-eb setenv BCRYPT_PASSWORD = $BCRYPT_PASSWORD
-eb setenv SALT_ROUNDS = $SALT_ROUNDS
-eb setenv JWT_SECRET = $JWT_SECRET
-eb setenv AWS_BUCKET= $AWS_BUCKET
-eb setenv AWS_REGION= $AWS_REGION
+eb setenv POSTGRES_HOST = echo $POSTGRES_HOST
+eb setenv POSTGRES_DB = echo $POSTGRES_DB
+eb setenv POSTGRES_USERNAME = echo $POSTGRES_USERNAME
+eb setenv POSTGRES_PASSWORD = echo $POSTGRES_PASSWORD
+eb setenv BCRYPT_PASSWORD = echo $BCRYPT_PASSWORD
+eb setenv SALT_ROUNDS = echo $SALT_ROUNDS
+eb setenv JWT_SECRET = echo $JWT_SECRET
+eb setenv AWS_BUCKET= echo $AWS_BUCKET
+eb setenv AWS_REGION= echo $AWS_REGION
 
 eb deploy
