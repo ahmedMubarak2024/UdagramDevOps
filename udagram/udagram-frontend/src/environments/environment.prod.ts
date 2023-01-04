45f12d11919b912d2db4aaff29721e50ba1a4dfb
Ahmed Mubarak <ahmed.mohammed2@vodafone.com>
2023-01-04 02:38:36 +0200

    first commit

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udagram-api-dev.eba-e5677xme.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
